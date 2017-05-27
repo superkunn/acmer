155946a28e96a799ea238b87e5253c58993e7345
superkunn <1150527505@qq.com>
2017-05-27 11:35:24 +0800

    poj dfs bfs

---
+++ poj/1562--Oil Deposits.cpp
@@ -0,0 +1,116 @@
+/*Oil Deposits
+Time Limit: 1000MS		Memory Limit: 10000K
+Total Submissions: 17675		Accepted: 9380
+Description
+
+The GeoSurvComp geologic survey company is responsible for detecting underground oil deposits. GeoSurvComp works with one large rectangular region of land at a time, and creates a grid that divides the land into numerous square plots. It then analyzes each plot separately, using sensing equipment to determine whether or not the plot contains oil. A plot containing oil is called a pocket. If two pockets are adjacent, then they are part of the same oil deposit. Oil deposits can be quite large and may contain numerous pockets. Your job is to determine how many different oil deposits are contained in a grid.
+Input
+
+The input contains one or more grids. Each grid begins with a line containing m and n, the number of rows and columns in the grid, separated by a single space. If m = 0 it signals the end of the input; otherwise 1 <= m <= 100 and 1 <= n <= 100. Following this are m lines of n characters each (not counting the end-of-line characters). Each character corresponds to one plot, and is either `*', representing the absence of oil, or `@', representing an oil pocket. 
+
+Output
+
+are adjacent horizontally, vertically, or diagonally. An oil deposit will not contain more than 100 pockets.
+Sample Input
+
+1 1
+*
+3 5
+*@*@*
+**@**
+*@*@*
+1 8
+@@****@*
+5 5 
+****@
+*@@*@
+*@**@
+@@@*@
+@@**@
+0 0
+Sample Output
+
+0
+1
+2
+2
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+
+char g[105][105];
+
+
+
+void dfs(int x,int y)
+{
+   if(g[x][y]=='@')
+   {
+       g[x][y]='1';
+
+       if(g[x][y+1]=='@')
+        dfs(x,y+1);
+       if(g[x+1][y+1]=='@')
+        dfs(x+1,y+1);
+       if(g[x+1][y]=='@')
+        dfs(x+1,y);
+       if(g[x+1][y-1]=='@')
+        dfs(x+1,y-1);
+       if(g[x][y-1]=='@')
+        dfs(x,y-1);
+       if(g[x-1][y-1]=='@')
+        dfs(x-1,y-1);
+       if(g[x-1][y]=='@')
+        dfs(x-1,y);
+       if(g[x-1][y+1]=='@')
+        dfs(x-1,y+1);
+
+   }
+}
+
+int main()
+{
+
+    int m,n;
+    while(cin>>m>>n&&m&&n)
+    {
+        int i,j;
+        for(i=1;i<=m;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                cin>>g[i][j];
+            }
+        }
+
+        int shu=0;
+        for(i=1;i<=m;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(g[i][j]=='@')
+                {
+                    dfs(i,j);
+                    shu++;
+                }
+
+            }
+
+        }
+
+
+        cout<<shu<<endl;
+
+
+
+    }
+
+
+
+    return 0;
+}
+
---
+++ poj/2251--Dungeon Master.cpp
@@ -0,0 +1,267 @@
+/*
+Dungeon Master
+Time Limit: 1000MS		Memory Limit: 65536K
+Total Submissions: 33998		Accepted: 12990
+Description
+
+You are trapped in a 3D dungeon and need to find the quickest way out! The dungeon is composed of unit cubes which may or may not be filled with rock. It takes one minute to move one unit north, south, east, west, up or down. You cannot move diagonally and the maze is surrounded by solid rock on all sides. 
+
+Is an escape possible? If yes, how long will it take? 
+Input
+
+The input consists of a number of dungeons. Each dungeon description starts with a line containing three integers L, R and C (all limited to 30 in size). 
+L is the number of levels making up the dungeon. 
+R and C are the number of rows and columns making up the plan of each level. 
+Then there will follow L blocks of R lines each containing C characters. Each character describes one cell of the dungeon. A cell full of rock is indicated by a '#' and empty cells are represented by a '.'. Your starting position is indicated by 'S' and the exit by the letter 'E'. There's a single blank line after each level. Input is terminated by three zeroes for L, R and C.
+Output
+
+Each maze generates one line of output. If it is possible to reach the exit, print a line of the form 
+Escaped in x minute(s).
+
+where x is replaced by the shortest time it takes to escape. 
+If it is not possible to escape, print the line 
+Trapped!
+Sample Input
+
+3 4 5
+S....
+.###.
+.##..
+###.#
+
+#####
+#####
+##.##
+##...
+
+#####
+#####
+#.###
+####E
+
+1 3 3
+S##
+#E#
+###
+
+0 0 0
+Sample Output
+
+Escaped in 11 minute(s).
+Trapped!
+*/
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+struct gg
+{
+    char fu;
+    int bu;
+};
+
+gg g[35][35][35];
+int ex,ey,ez;
+
+struct biao
+{
+    int xx;
+    int yy;
+    int zz;
+    int step;
+};
+
+biao dui[100000];
+int head=0,tail=1;
+int shu=0;
+
+
+
+void bfs(int x,int y,int z)
+{
+
+
+    if((g[x+1][y][z].fu=='.'||g[x+1][y][z].fu=='E')&&g[x+1][y][z].bu==0)
+    {
+        if(g[x+1][y][z].fu=='E')
+        {
+
+            cout<<"Escaped in "<<dui[head].step+1<<" minute(s)."<<endl;
+            head=tail-1;
+            shu=1;
+            return;
+
+        }
+        dui[tail].xx=x+1;
+        dui[tail].yy=y;
+        dui[tail].zz=z;
+        dui[tail].step=dui[head].step+1;
+        tail++;
+        g[x+1][y][z].bu=1;
+
+    }
+    if((g[x-1][y][z].fu=='.'||g[x-1][y][z].fu=='E')&&g[x-1][y][z].bu==0)
+    {
+        if(g[x-1][y][z].fu=='E')
+        {
+
+            cout<<"Escaped in "<<dui[head].step+1<<" minute(s)."<<endl;
+            head=tail-1;
+            shu=1;
+            return;
+
+        }
+        dui[tail].xx=x-1;
+        dui[tail].yy=y;
+        dui[tail].zz=z;
+        dui[tail].step=dui[head].step+1;
+        tail++;
+        g[x-1][y][z].bu=1;
+    }
+    if((g[x][y+1][z].fu=='.'||g[x][y+1][z].fu=='E')&&g[x][y+1][z].bu==0)
+    {
+        if(g[x][y+1][z].fu=='E')
+        {
+
+            cout<<"Escaped in "<<dui[head].step+1<<" minute(s)."<<endl;
+            head=tail-1;
+            shu=1;
+            return;
+
+        }
+        dui[tail].xx=x;
+        dui[tail].yy=y+1;
+        dui[tail].zz=z;
+        dui[tail].step=dui[head].step+1;
+        tail++;
+        g[x][y+1][z].bu=1;
+    }
+    if((g[x][y-1][z].fu=='.'||g[x][y-1][z].fu=='E')&&g[x][y-1][z].bu==0)
+    {
+        if(g[x][y-1][z].fu=='E')
+        {
+
+            cout<<"Escaped in "<<dui[head].step+1<<" minute(s)."<<endl;
+            head=tail-1;
+            shu=1;
+            return;
+
+        }
+        dui[tail].xx=x;
+        dui[tail].yy=y-1;
+        dui[tail].zz=z;
+        dui[tail].step=dui[head].step+1;
+        tail++;
+        g[x][y-1][z].bu=1;
+    }
+    if((g[x][y][z+1].fu=='.'||g[x][y][z+1].fu=='E')&&g[x][y][z+1].bu==0)
+    {
+        if(g[x][y][z+1].fu=='E')
+        {
+
+            cout<<"Escaped in "<<dui[head].step+1<<" minute(s)."<<endl;
+            head=tail-1;
+            shu=1;
+            return;
+
+        }
+        dui[tail].xx=x;
+        dui[tail].yy=y;
+        dui[tail].zz=z+1;
+        dui[tail].step=dui[head].step+1;
+        tail++;
+        g[x][y][z+1].bu=1;
+    }
+    if((g[x][y][z-1].fu=='.'||g[x][y][z-1].fu=='E')&&g[x][y][z-1].bu==0)
+    {
+        if(g[x][y][z-1].fu=='E')
+        {
+
+            cout<<"Escaped in "<<dui[head].step+1<<" minute(s)."<<endl;
+            head=tail-1;
+            shu=1;
+            return;
+
+        }
+        dui[tail].xx=x;
+        dui[tail].yy=y;
+        dui[tail].zz=z-1;
+        dui[tail].step=dui[head].step+1;
+        tail++;
+        g[x][y][z-1].bu=1;
+    }
+
+}
+
+int main()
+{
+
+
+    int x,y,z;
+    while(cin>>x>>y>>z&&x&&y&&z)
+    {
+        int i,j,k;
+
+        for(i=1;i<=x;i++)
+        {
+            for(j=1;j<=y;j++)
+            {
+                for(k=1;k<=z;k++)
+                {
+                    cin>>g[i][j][k].fu;
+                    g[i][j][k].bu=0;
+
+                    if(g[i][j][k].fu=='S')
+                    {
+                        dui[head].xx=i;
+                        dui[head].yy=j;
+                        dui[head].zz=k;
+                        dui[head].step=0;
+
+                    }
+                    if(g[i][j][k].fu=='E')
+                    {
+                        ex=i;
+                        ey=j;
+                        ez=k;
+                    }
+                }
+            }
+        }//put in
+
+
+        while(head!=tail)
+        {
+
+            bfs(dui[head].xx,dui[head].yy,dui[head].zz);
+            head++;
+        }
+
+        if(shu==0)
+        {
+            cout<<"Trapped!"<<endl;
+        }
+
+
+        head=0;
+        tail=1;
+        shu=0;
+
+        for(i=0;i<100000;i++)
+        {
+            dui[i].step=0;
+        }
+
+
+
+
+
+
+
+
+    }//end while
+
+    return 0;
+}
+
+
